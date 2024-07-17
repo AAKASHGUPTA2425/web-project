cb25f28c704e6a4316daac03afbee3dcaa14fb7f
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-17 10:50:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+document.getElementById('toglebutton').addEventListener('click', function() {
+    var img = document.getElementById('mer');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('venbutton').addEventListener('click', function() {
+    var img = document.getElementById('ven');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('earthbutton').addEventListener('click', function() {
+    var img = document.getElementById('earth');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('marbutton').addEventListener('click', function() {
+    var img = document.getElementById('mar');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('jupbutton').addEventListener('click', function() {
+    var img = document.getElementById('jup');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('satbutton').addEventListener('click', function() {
+    var img = document.getElementById('sat');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('urabutton').addEventListener('click', function() {
+    var img = document.getElementById('ura');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
+document.getElementById('nepbutton').addEventListener('click', function() {
+    var img = document.getElementById('nep');
+    if (img.classList.contains('hidden')) {
+        img.classList.remove('hidden');
+    } else {
+        img.classList.add('hidden');
+    }
+});
